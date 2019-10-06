e3b2fa2c26f6f719504952dee51fd56d4c91d362
audombek <audreyombek@gmail.com>
2019-10-06 23:55:01 +0300

    add an function to collect user input from contact form

--- js/script.js
+++ js/script.js
@@ -1,89 +1,75 @@
-//constructor
-var totalCosts=[];
-function order(size, crust, name, topping){
+function order(size, crust,topping ){
     this.size = size;
     this.crust = crust;
-    this.name = name;
-    this.topping = topping;
+    this.topping =topping;
     this.price = 0;
+
 }
+var pizzaSize = ["Large", "Standard", "Small"];
+var pizzaCrust = ["Crispy Crust","Stuffed-Crust","Glutten-free Crust"];
+var pizzaTopping =["Large:Chicken ksh 500","Large:Meat ksh 450","Large:Vegetables ksh 400"]
+var totalCosts = [];
 
-var pizzaName =["Chicken","Vegetables","Meat"]
-var pizzaSize=["Large","Small","Medium"]
-var pizzaTopping=["Vegetable toppings","Extra meat toppings","Extra Chicken topping"]
-var pizzaCrust=["Thin Crust","Thick Crust","Deep Crust"]
-//prototype
-order.prototype.cost= function(){
-    if(this.size === pizzaSize[0]){
-        this.price += 550;
+order.prototype.cost = function(){
+    if (this.size === pizzaSize[0]){ 
+        this.price += 1000;
     }
-    else if(this.size=== pizzaSize[1]){
-        this.price += 450;
+    else if (this.size === pizzaSize[1]){
+        this.price +=800;
     }
-    else if(this.size === pizzaSize[2]){
-        this.price +=500;
+    else if (this.size === pizzaSize[2]){
+        this.price += 600;
     }
 
-
-    if(this.topping === pizzaTopping[0]){
-        this.price += 100
-    }
-    else if(this.topping === pizzaTopping[1]){
-        this.price += 100;
-    }
-    else if(this.topping === pizzaTopping[2]){
+    if (this.crust === pizzaCrust[0]){
         this.price += 150;
     }
-
-
-    if(this.crust===pizzaCrust[0]){
-        this.price += 100;
-    }
-    else if(this.crust === pizzaCrust[1]){
-        this.price += 150;
-    }
-    else if(this.crust === pizzaCrust[2]){
+    else if (this.crust === pizzaCrust[1]){
         this.price += 200;
     }
-
-    return this.price;
+    else if (this.crust === pizzaCrust[2]){
+        this.price += 250;
     }
-    order.prototype.totalCost = function(){
-        var orderTotal = 0;
-        for(var order =0; order < totalCosts.length; order++){
-            orderTotal += totalCosts[order];
-        }
-        return orderTotal;
-     }
 
+    if (this.topping === pizzaTopping[0]){
+        this.price += 500;
+    }
+     else if (this.topping === pizzaTopping[1]){
+        this.price += 450;
+    }
+     else if (this.topping === pizzaTopping[2]){
+        this.price += 400;
+    }
+     
+     return this.price;
 
-// ui logics
-$(document).ready(function(){
-    $("button#submit").click(function(event){
-        event.preventDefault();
-        var size = $("#size").val();
-        var crust =$("#crust").val();
-        var topping =$("#topping").val();
-        var name =$("#name").val();
-        var newPizzaOrder = new order (size, crust, topping);
-       newPizzaOrder.cost();
-       totalCosts.push(newPizzaOrder.price);
-       
-       alert(totalCosts);
-       
-    });
-
-   
-});
-var expanded = false;
-
-function showCheckboxes() {
-  var checkboxes = document.getElementById("checkboxes");
-  if (!expanded) {
-    checkboxes.style.display = "block";
-    expanded = true;
-  } else {
-    checkboxes.style.display = "none";
-    expanded = false;
-  }
 }
+order.prototype.totalCost = function(){
+    var orderTotal = 0;
+    for(var order =0; order < totalCosts.length; order++){
+        orderTotal += totalCosts[order];
+    }
+    return orderTotal;
+}
+
+$(document).ready(function () {
+    $("button#submit").click(function (event) { 
+        event.preventDefault();
+        var sizes = $("select#size").val();
+        var crusts = $("select#crust").val();
+        var toppings = $("select#topping").val();
+        
+
+        var newPizzaOrder = new order(sizes, crusts,toppings);
+        newPizzaOrder.cost();
+        totalCosts.push(newPizzaOrder.price);
+        alert(totalCosts);
+
+        }); 
+
+        $(".frm1").submit(function(){
+            var name =$("input#name").val();
+            alert(name+" we have received your message. Thank you for communicating back to us.");
+        });
+        
+});
