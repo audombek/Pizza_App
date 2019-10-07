c7269d50191353c2bd2bf7ebf3d4d7c004e1824b
audombek <audreyombek@gmail.com>
2019-10-07 11:03:33 +0300

    add .text method to checkout button

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-function order(size, crust,topping ){
+var totalCosts = [];
+
+function order(size, crust,meatTopping,vegetableTopping ){
     this.size = size;
     this.crust = crust;
-    this.topping =topping;
+    this.meatTopping =meatTopping;
+    this.vegetableTopping=vegetableTopping;
     this.price = 0;
 
 }
 var pizzaSize = ["Large", "Standard", "Small"];
 var pizzaCrust = ["Crispy Crust","Stuffed-Crust","Glutten-free Crust"];
-var pizzaTopping =["Large:Chicken ksh 500","Large:Meat ksh 450","Large:Vegetables ksh 400"]
-var totalCosts = [];
+var pizzameatTopping =["Chicken","Beef","Boerwors"];
+var pizzavegetableTopping=["Onion","Brocolli","Spinach"];
+
 
 order.prototype.cost = function(){
     if (this.size === pizzaSize[0]){ 
@@ -31,19 +35,29 @@ order.prototype.cost = function(){
         this.price += 250;
     }
 
-    if (this.topping === pizzaTopping[0]){
+    if (this.meatTopping === pizzameatTopping[0]){
         this.price += 500;
     }
-     else if (this.topping === pizzaTopping[1]){
+     else if (this.meatTopping === pizzameatTopping[1]){
         this.price += 450;
     }
-     else if (this.topping === pizzaTopping[2]){
+     else if (this.meatTopping === pizzameatTopping[2]){
         this.price += 400;
+    }
+    if (this.vegetableTopping === pizzavegetableTopping[0]){
+        this.price += 100;
+    }
+     else if (this.vegetableTopping === pizzavegetableTopping[1]){
+        this.price += 100;
+    }
+     else if (this.vegetableTopping === pizzavegetableTopping[2]){
+        this.price += 100;
     }
      
      return this.price;
 
 }
+
 order.prototype.totalCost = function(){
     var orderTotal = 0;
     for(var order =0; order < totalCosts.length; order++){
@@ -53,23 +67,36 @@ order.prototype.totalCost = function(){
 }
 
 $(document).ready(function () {
-    $("button#submit").click(function (event) { 
+    $("form").submit(function (event) { 
         event.preventDefault();
         var sizes = $("select#size").val();
         var crusts = $("select#crust").val();
-        var toppings = $("select#topping").val();
-        
-
-        var newPizzaOrder = new order(sizes, crusts,toppings);
+        var meatTopping = $("select#meattopping").val();
+        var vegetableTopping=$("select#veggtopping").val();
+        var newPizzaOrder = new order(sizes, crusts,meatTopping,vegetableTopping );
         newPizzaOrder.cost();
         totalCosts.push(newPizzaOrder.price);
-        alert(totalCosts);
-
-        }); 
-
-        $(".frm1").submit(function(){
-            var name =$("input#name").val();
-            alert(name+" we have received your message. Thank you for communicating back to us.");
-        });
+        $("#pz").text(sizes);
+        $("#cr").text(crusts);
+        $("#mt").text(meattopping);
+        $("#vt").text(veggtopping);
+        $("#tc").text(newPizzaOrder.totalCost());
+ 
+        
+        $("button#order").click(function(){
+            alert("Your Order has been received.Please click delivery and if you want your pizza to be delivered.");
+        $("button#delivery").click(function(){
+            prompt("Enter your location");
+            alert("Your delivery charge is ksh 150.");
+            alert("Your pizza will be delivered at your location.");
+        })  
+        })
+    
+  });
+     
+     $(".sub1 button#submit1").click(function(){ 
+        var name =$("input#name").val();
+        alert(name+" we have received your message. Thank you for reaching out to us.");
+    });
         
 });
